517ed468d18033a2c85356ee4159d80addf0c1da
smhrd <smhrd@121.147.52.104>
2021-07-27 08:55:54 +0900

    올라갔니?

--- plogging/src/Model/memberDAO.java
+++ plogging/src/Model/memberDAO.java
@@ -1,8 +1,76 @@
 package Model;
 
-public class memberDAO {
-private void syso() {
-	// TODO Auto-generated method stub
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-}
+import Model.memberDTO;
+
+public class memberDAO {
+	Connection conn = null;
+	PreparedStatement psmt = null;
+	ResultSet rs = null;
+	
+	//
+	int cnt = 0;
+	
+	public void conn() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String dbid = "hr";
+			String dbpw = "hr";
+
+			conn = DriverManager.getConnection(url, dbid, dbpw);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void close() {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (psmt != null) {
+				psmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public int join(memberDTO dto) {
+		try {
+			// DB 연결 메소드 호출
+			conn();
+
+			String sql = "insert into web_member values(?, ?,?,?, ?, ?)";
+
+			psmt = conn.prepareStatement(sql);
+
+			psmt.setString(1, dto.getId());
+			psmt.setString(2, dto.getPw());
+			psmt.setString(3, dto.getName());
+			psmt.setString(4, dto.getAge());
+			psmt.setString(5, dto.getAddr());
+			psmt.setString(6, dto.getPlogging());
+
+			cnt = psmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			// DB 문 닫는 메소드 호출
+			close();
+		}
+		return cnt;
+	}
 }
