84261f5225582e23752912cce375cccd777f09c6
SMHRD2 <SMHRD2@121.147.52.230>
2021-07-26 17:14:53 +0900

    공고목록작성

--- plogging/src/Model/notic_boardDTO.java
+++ plogging/src/Model/notic_boardDTO.java
@@ -2,4 +2,62 @@ package Model;
 
 public class notic_boardDTO {
 
+	private String location;
+	private String ploggingDate;
+	private String postPhoto;
+	private String peopleLimit;
+	private String postNumber;
+	private String list;
+	
+	
+	public notic_boardDTO(String location, String ploggingDate, String postPhoto, String peopleLimit, String postNumber,
+			String list) {
+		super();
+		this.location = location;
+		this.ploggingDate = ploggingDate;
+		this.postPhoto = postPhoto;
+		this.peopleLimit = peopleLimit;
+		this.postNumber = postNumber;
+		this.list = list;
+	}
+	
+	
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getPloggingDate() {
+		return ploggingDate;
+	}
+	public void setPloggingDate(String ploggingDate) {
+		this.ploggingDate = ploggingDate;
+	}
+	public String getPostPhoto() {
+		return postPhoto;
+	}
+	public void setPostPhoto(String postPhoto) {
+		this.postPhoto = postPhoto;
+	}
+	public String getPeopleLimit() {
+		return peopleLimit;
+	}
+	public void setPeopleLimit(String peopleLimit) {
+		this.peopleLimit = peopleLimit;
+	}
+	public String getPostNumber() {
+		return postNumber;
+	}
+	public void setPostNumber(String postNumber) {
+		this.postNumber = postNumber;
+	}
+	public String getList() {
+		return list;
+	}
+	public void setList(String list) {
+		this.list = list;
+	}
+
+	
 }
