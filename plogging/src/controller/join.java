2af592862edb6ddb07a98f4ca5e4c7f687101335
smhrd <smhrd@121.147.52.104>
2021-07-27 09:32:16 +0900

    조인수정

--- plogging/src/controller/join.java
+++ plogging/src/controller/join.java
@@ -6,12 +6,48 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Model.memberDAO;
+import Model.memberDTO;
 
 @WebServlet("/join")
 public class join extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-	}
+			request.setCharacterEncoding("EUC-KR");
 
-}
+			String id = request.getParameter("id");
+			String pw = request.getParameter("pw");
+			String name = request.getParameter("name");
+			String age = request.getParameter("age");
+			String addr = request.getParameter("addr");
+			String plogging = request.getParameter("plogging");
+
+			System.out.println("id : "+id);
+			System.out.println("pw : "+pw);
+			System.out.println("name : "+name);
+			System.out.println("age : "+age);
+			System.out.println("addr : "+addr);
+			System.out.println("plogging : "+plogging);
+
+			memberDTO dto = new memberDTO(id, pw, name, age,addr, plogging);
+			memberDAO dao = new memberDAO();
+			int cnt = dao.join(dto);
+
+
+			String moveURL = "";
+			if(cnt>0) {
+				System.out.println("회원가입성공");
+				moveURL = "";
+				HttpSession session = request.getSession();
+				session.setAttribute("id", id);
+			}else {
+				System.out.println("회원가입실패");
+				moveURL = "";
+			}
+
+			response.sendRedirect(moveURL);
+		}
+	}
