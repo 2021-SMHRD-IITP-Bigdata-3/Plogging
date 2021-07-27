efb44bc0f8bf2e35edc129184f1d1ea291150f2a
SMHRD2 <SMHRD2@121.147.52.230>
2021-07-27 18:25:45 +0900

    멤버DTO수정

--- plogging/src/Model/memberDTO.java
+++ plogging/src/Model/memberDTO.java
@@ -35,8 +35,6 @@ public class memberDTO {
 
 
 
-
-
 	public memberDTO(String memberId, String memberPw, String memberName, int memberAge, String memberAddr,
 			String memberPlogCount) {
 		super();
